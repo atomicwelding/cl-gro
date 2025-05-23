c2881bfa6187e4569d742745beffa870b460f1f4
atomicwelding <ctfweld@gmail.com>
2025-05-23 22:00:39 +0200

    rewrite the write-gro

--- writer.lisp
+++ writer.lisp
@@ -3,6 +3,26 @@
 
 (defmethod write-gro ((system system) stream)
   (format stream "~a~%" (system-title system))
-  (format stream "~5d~%" 0.0)
+  (format stream "~5d~%" (number-atoms system)) ;; number of atoms
+  (vdestructuring-bind (length width height) (system-box-size system)
+    (format stream "~10,5f~10,5f~10,5f~%" length width height)))
+
+
+
+
+
+(defmethod write-gro ((system system) stream)
+  (format stream "~a~%" (system-title system))	; title
+  (format stream "~5d~%" (number-atoms system)) ; nb of atoms
+  (loop for residue in (system-residues system)
+        for resid from 1
+        for resname = (residue-name residue)
+        do (loop for atom in (residue-atoms residue)
+                 for atomid from 1 by 1
+                 for atomname = (single-atom-name atom)
+                 for pos = (single-atom-position atom)
+                 do (vdestructuring-bind (x y z) pos
+                      (format stream "~5d~5a~5a~5d~8,3f~8,3f~8,3f~%"
+                              resid resname atomname atomid x y z))))
   (vdestructuring-bind (length width height) (system-box-size system)
     (format stream "~10,5f~10,5f~10,5f~%" length width height)))
